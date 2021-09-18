aa460bdb82ed3cd02ce97c9ecb3aff538b7dc58f
Anthony Turner <225599+anthturner@users.noreply.github.com>
2021-09-18 16:23:11 -0400

    code

---
+++ SecOpsSteward.Plugins/ManagedServiceAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SecOpsSteward.Plugins
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ManagedServiceAttribute : Attribute
+    {
+        public ManagedServiceAttribute(Type serviceType)
+        {
+            ManagedServiceType = serviceType;
+            ManagedServiceId = serviceType.GenerateId();
+        }
+
+        public Type ManagedServiceType { get; set; }
+        public Guid ManagedServiceId { get; }
+    }
+}
