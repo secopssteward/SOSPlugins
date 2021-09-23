34a4e85d8480de41f24b18679ae3ce435a6a5ee1
Anthony Turner <225599+anthturner@users.noreply.github.com>
2021-09-23 13:53:16 -0400

    add image

---
+++ SecOpsSteward.Plugins/ServiceImageAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SecOpsSteward.Plugins
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ServiceImageAttribute : Attribute
+    {
+        public string SVG { get; set; }
+
+        public ServiceImageAttribute() { }
+        public ServiceImageAttribute(string svg) => SVG = svg;
+    }
+}
